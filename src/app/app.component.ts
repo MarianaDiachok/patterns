05474ac0674a63a59e0c92f20861506f088556c0
Mariana_Diachok <mxd2@cbord.com>
2020-04-13 16:38:57 +0300

    created observer and strategy examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {MessageService} from './services/message.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'patterns';
+  messages: any[] = [];
+  subscription: Subscription;
+
+  constructor(private messageService: MessageService) {
+    this.subscription = this.messageService.getMessage().subscribe(message => {
+      if (message) {
+        this.messages.push(message);
+      } else {
+        this.messages = [];
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ObservableComponent } from './behavioural/observable/observable.component';
+import { ObserverTypescriptComponent } from './behavioural/observer-typescript/observer-typescript.component';
+import { StrategyComponent } from './behavioural/strategy/strategy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ObservableComponent,
+    ObserverTypescriptComponent,
+    StrategyComponent
   ],
   imports: [
     BrowserModule,
